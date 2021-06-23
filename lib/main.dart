57b24daf03ac80f7173a445e9310210b4c037b6e
Leandro Almeida <leandrosalmeida20@gmail.com>
2021-06-22 22:13:11 -0300

    Configuração de inicialização do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pay_flow/modules/home/home_page.dart';
-import 'package:pay_flow/modules/login/login_page.dart';
-import 'package:pay_flow/modules/splash/splash_page.dart';
-import 'package:pay_flow/shared/themes/app_colors.dart';
+import 'package:pay_flow/app_widget.dart';
 
 void main() {
   runApp(AppWidget());
 }
 
-class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+class AppFireBase extends StatefulWidget {
+  const AppFireBase({Key? key}) : super(key: key);
 
   @override
+  _AppFireBaseState createState() => _AppFireBaseState();
+}
+
+class _AppFireBaseState extends State<AppFireBase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Pay Flow",
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Material(
+            child: Center(
+              child: Text("Não foi possível inicializar o Firebase"),
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
